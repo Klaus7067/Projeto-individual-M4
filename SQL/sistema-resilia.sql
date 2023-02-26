fab85b2ea8caed2c7f324eb6dbf27bc7ac9bfc28
Klaus7067 <riansouza589@gmail.com>
2023-02-26 19:55:46 -0300

    Update SQL

--- SQL/sistema-resilia.sql
+++ SQL/sistema-resilia.sql
@@ -1,9 +1,21 @@
 use resilia;
 create table cursos(
 	id_cursos int primary key auto_increment,
-    curso varchar(100) not null
+    nome_curso varchar(100),
+    curso varchar(100) not null,
+    valor_curso int,
+    duracao_curso int
 )
 create table turmas(
 	id_turmas int primary key auto_increment,
-    nome varchar(100) not null
+    nome varchar(100) not null,
+    id_aluno int,
+    id_professor int,
+    id_materia int
+)
+create table alunos(
+	id_alunos int primary key auto_increment,
+    nome_aluno varchar(100) not null,
+    numero_aluno varchar(11),
+    data_nascimento datetime 
 )
